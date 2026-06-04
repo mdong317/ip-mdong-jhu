9104cd59ef7c35963a4852c15c72cc420a458f9e
mdong317 <michelledong317@gmail.com>
2026-06-04 15:02:56 -0400

    Update bronze.c

--- homework/hw0/bronze.c
+++ homework/hw0/bronze.c
@@ -1,1 +1,8 @@
+//michelle dong
+//mdong16
+#include <stdio.h>
 
+int main() {
+    printf("Team Kenya earns Bronze in the mixed 4 by 400m relay, celebrating unity through sport.\n");
+    return 0;
+}
